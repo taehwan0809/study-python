c859222bf4698d44e130d1a26fb7cf91538be7b8
taehwan <dcpop28201004@gmail.com>
2025-08-22 10:01:06 +0900

    today study

--- study.py
+++ study.py
@@ -96,3 +96,16 @@
 # print(x)
 
 
+#11. a,b = map(int, input().split())
+# c = a+b
+# if a%2==0 and b%2==0 and c%2==0:
+#     print("짝수+짝수=짝수")
+# elif a%2 ==0 and b%2 != 0 and c%2!=0:
+#     print('짝수+홀수=홀수')
+# elif a%2 != 0 and b%2 != 0 and c%2==0:
+#     print('홀수+홀수=짝수')
+# elif a%2 != 0 and b%2 ==0 and c%2!=0:
+#     print('홀수+짝수=홀수')
+
+
+
